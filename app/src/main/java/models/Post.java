338c632dc6bc8ed3587a1732317dac2496a287c2
Kevin321an <kevin321an@gmail.com>
2016-06-27 12:48:38 -0400

    define the models, and wirting the model to the cloud

---
+++ app/src/main/java/models/Post.java
@@ -0,0 +1,47 @@
+package models;
+
+import java.util.HashMap;
+import java.util.Map;
+import com.google.firebase.database.Exclude;
+import com.google.firebase.database.IgnoreExtraProperties;
+/**
+ * Created by FM on 6/27/2016.
+ */
+@IgnoreExtraProperties
+public class Post {
+        public String uid;
+        public String author;
+        public String title;
+        public String body;
+        public String url;
+        public int starCount = 0;
+        public Map<String, Boolean> stars = new HashMap<>();
+
+        public Post() {
+            // Default constructor required for calls to DataSnapshot.getValue(Post.class)
+        }
+
+        public Post(String uid, String author, String title, String body, String url) {
+            this.uid = uid;
+            this.author = author;
+            this.title = title;
+            this.body = body;
+            this.url = url;
+
+        }
+
+        // [START post_to_map]
+        @Exclude
+        public Map<String, Object> toMap() {
+            HashMap<String, Object> result = new HashMap<>();
+            result.put("uid", uid);
+            result.put("author", author);
+            result.put("title", title);
+            result.put("body", body);
+            result.put("url", url);
+            result.put("starCount", starCount);
+            result.put("stars", stars);
+            return result;
+        }
+        // [END post_to_map]
+}
